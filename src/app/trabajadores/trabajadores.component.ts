965e607036753a1b48b07030aad4d87890bc3c43
Darwin <floresjack74@gmail.com>
2025-04-02 19:22:58 -0500

    componenetes actualizados

--- src/app/trabajadores/trabajadores.component.ts
+++ src/app/trabajadores/trabajadores.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trabajadores',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './trabajadores.component.html',
   styleUrl: './trabajadores.component.css'
 })
-export class TrabajadoresComponent {
+export class TrabajadoresComponent implements OnInit{
+  data:any;
 
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+    console.log(this.fetchData())
+  }
+
+  fetchData(){
+    this.http.get("restaurant-api-gold.vercel.app/api/usuarios")
+    .subscribe(respuesta=>{
+      this.data = respuesta
+      console.log(this.data)
+    })
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
+// Asegúrate de incluir 'provideHttpClient()' en los providers de la configuración
+bootstrapApplication(AppComponent, {
+  ...appConfig, // Mantén la configuración existente
+  providers: [
+    ...appConfig.providers,  // Si ya tienes otros proveedores, manténlos
+    provideHttpClient()      // Agrega HttpClient aquí
+  ]
+})
   .catch((err) => console.error(err));
